a27ee75d3098e7f3bdf2e7282e3660c3655161bc
sunbinkwon2 <sunbinkwon2@gmail.com>
2026-05-28 09:50:51 +0900

    [refactoring] TDD 4

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -3,14 +3,14 @@ import java.util.List;
 
 public class PrimeFactor {
 
-    public static List<Integer> generate(int number) {
+    public static List<Integer> generate(int n) {
         List<Integer> factors = new ArrayList<>();
-        while (number % 2 == 0) {
+        while (n % 2 == 0) {
             factors.add(2);
-            number /= 2;
+            n /= 2;
         }
-        if (number > 1) {
-            factors.add(number);
+        if (n > 1) {
+            factors.add(n);
         }
         return factors;
     }
